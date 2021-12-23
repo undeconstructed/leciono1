861752243e87743fd260df2373d4e8eff0c2dcaa
Phil Housley <undeconstructed@gmail.com>
2021-12-23 16:46:42 +0000

    Translatable and some styling

--- js/main.js
+++ js/main.js
@@ -1,21 +1,201 @@
 
-function main() {
-  let setupAudio = (e) => {
-    let id = e.getAttribute('fid')
-    let eAudio = document.createElement('audio')
-    eAudio.src = `sound/e${id}.mp3`
-    e.append(eAudio)
-    e.addEventListener('click', _e => {
-      eAudio.play()
-    })
-    eAudio.addEventListener('play', _e => {
-      e.classList.add('playing')
-    })
-    eAudio.addEventListener('ended', _e => {
-      e.classList.remove('playing')
-    })
-    e.append('🔉\ufe0e')
+const translations = {
+  "eo": {
+    "title": "Esperanto en 17 minutoj",
+    "01": "(Ĉi tio ne estas Esperanto-kurso, sed ĝi donu al vi ideon pri kio la lingvo estas, kaj vi verŝajne povus lerni la plimulton de tio kion vi bezonas per ĉi tio, kaj per iom da legado.)",
+    "02": "Esperanto estas aro da vortradikoj, kaj sistemo por aranĝi ilin por ke oni povu uzi ilin por krei ion pli grandan. Ni komencu.",
+    "03": "Rigardu tra la fenestro. Verŝajne pluvas. La radiko por tio estas <em>pluv-</em>.",
+    "04": "Pluvo estas kialo por paroli. Por diri ke pluvas, prenu la radiko \"pluv-\", kaj verbigu ĝin, per aldono de la finaĵo kiu signifas \"okazas nun\": <em>-as</em>.",
+    "e01": "pluvas.",
+    "05": "Tio estas frazo. Oni konstruas frazon ĉirkaŭ verbo - tiu verbo enhavas la kialon por fari la frazon, kaj marko kiu diras kiam tiu afero okazas. Estas kvin finaĵoj:",
+    "verb-is": "okazis en la pasinteco",
+    "verb-as": "okazas nun",
+    "verb-os": "verŝajne okazos iam en la estonteco",
+    "verb-us": "okazas en imaga mondo",
+    "verb-u": "iu volas, ke ĝi okazu",
+    "06": "Kelkfoje, oni volas diri kiu aŭ kio faras la verbon. Tio estas tre facila - prenu la radiko de tiu afero, kaj substantivigu ĝin per la <em>-o</em> finaĵo.",
+    "e02": "kato kuris",
+    "07": "Se oni volas uzi alian koncepton por priskribi substantivon, prenu la radikon por tiu koncepto, kaj aldonu <em>-a</em> por fari ĝin adverbo:",
+    "e03": "blua kato dormos.",
+    "08": "Eble estas alia koncepto kiu iel rilatas al ĉi tiu ago. Eble estas loko, en kiu ĝi okazas. Por aldoni alian substantivon, oni povas enkonduki ĝin per prepozicion, por diri kiel ĝi rilatas al la ago.",
+    "e04": "blua kato sidas antaŭ televidilo (ilo-por-televidi).",
+    "09": "Kelkfoje ne estas evidente elektinda prepozicio, aŭ vi nur ne volas uzi ĝin. Tiaokaze vi povas marki la substantivon (kaj ĝiajn adjektivojn) per <em>-n</em> finaĵo, por diri \"vi scias pri kio estas ĉi tiu afero\".",
+    "e05": "kateto ne ŝatas pluvegon.",
+    "10": "Parenteze, ĉar ĉio estas nete markita, oni povas iom reaordigi la vortojn:",
+    "e06": "pluvon malvarman (malo-de-varma) kato malamas (malo-de-amas).",
+    "11": "Sed ne tro faru tion, nur uzi ĝin kiam ĝi plifaciligas komprenon de la frazo.",
+    "12": "Estas nur unu plia speco de verbo por lerni: adverboj. Mi ne scias precize kio estas adverbo, sed ĝi finiĝas per <em>-e</em> kaj oni uzas ĝin por priskribi ion ajn kiu ne estas substantivo. Ili estas tre flekseblaj (eble dangere!), sed komencu per meti ilin antaŭ la vorto kiun ili priskribas, kaj vi baldaŭ komprenos la ideon:",
+    "e07": "la subite malseka (malo-de-seka) kato rapide enkuras (en-kuras).",
+    "13": "Foje oni devas esti pli precize ol nur \"kato\" - oni dfevas diri kiu kato. La plej facila eblo estas uzi <em>la</em>, kiu surbaze signifas \"vi scias kiun.\"",
+    "e08": "la kato manĝas manĝaĵojn (aĵon-por-manĝi).",
+    "14": "Aŭ eble oni pli volas diri \"tiu, pri kiu ni parolas\":",
+    "e09": "tiu kato ĉasus hundojn.",
+    "15": "Se oni volas rilatigi kelkajn frazojn, oni bezonas vortojn por fari tion. Oni ĉiam havos unu ĉefan frazon, kiel antaŭe, sed rilataj frazoj bezonos enkondukon, ekzemple:",
+    "e10": "tiu kato ĉasus hundojn, se ĝi trovus hundojn.",
+    "e11": "tiu kato malŝatas hundojn, ĉar ili estas bruaj.",
+    "16": "Dum ni tiel emas, ni faru ĉiujn el la pronomoj:",
+    "mi": "la ulo kiu parolas",
+    "ni": "la parolanto kaj ties kunuloj",
+    "vi": "al kiu, tiu ulo parolas",
+    "gxi": "alia aĵo",
+    "li": "alia iĉa ulo",
+    "sxi": "alia ina ulo",
+    "ili": "plruaj aliaj aĵoj aŭ uloj",
+    "oni": "simple, iu",
+    "17": "\"Sed\", vi demandas, \"kio pri seks-neŭtraleco?\". Nu, ni revenos al tiu temo, poste.",
+    "18": "Okej, nun venas malfacila parto. Ĉu vi estas preta? Kelkfoje oni volas ligi frazojn laŭ speciala maniero, kie oni prenas ideon de unu frazo kaj iel metas ĝin en la alian. Por tio, oni uzas la vorton <em>kiu</em>, kiu estas kiel <em>tiu</em>, sed estas speciale por citi ideon en nova frazo:",
+    "e12": "la kato, kiu ĉasas hundojn, estas feliĉa.",
+    "19": "Hm, ĉu estas sufiĉe klare, ke la \"kiu\" estas pri la kato? Jes, verŝajne, sed ni povas esti pli klara:",
+    "e13": "la kato, kiu kato estas granda, estas dormema (ema-al-dormo).",
+    "20": "Fakte, oni povas preni iun ajn ideojn el la ĉefa frazo, kaj meti ĝin en subfrazon, ni nur devas diri kian aferon ĝi estas:",
+    "e14": "mi faris tion je la oka horo, je kiu tempo mi ridis.",
+    "21": "Fakte, ekzistas kelkaj faciligoj kiujn oni povas uzi por ĉi tiaj aferoj:",
+    "e15": "mi estas kontenta, kiam mi estas kun vi.",
+    "22": "Ni parolas pri ĉiuj el tiaj vortoj iam poste, sed por nun jen nur unu plian: Foje oni simple volas paroli pri .. io. Ne estas klare, kia afero ĝi estas tiu afero, ĝi simple estas io. Ni provu tion:",
+    "e16": "kio estas tio? tio estas kato.",
+    "23": "Ho, jes, ankaŭ tiuj <em>ki-</em> vortoj estas bonaj por demandoj. Pensu ke ili signifas \"metu la taŭgan aferon ĉi tien, mi petas\". Ankaŭ ekzistas simplan rimedon por starigi jes/ne demandon:",
+    "e17": "ĉu vi komprenas? jes .. kaj ne.",
+    "24": "Bone. Estas nur unu plian aferon por diskuti. No komencos facile, kaj tiam iĝu freneza kun ĝi. Se oni volas priskribi ion dufoje, ekzemple per speco kaj per nomo, simple metu ilin ambaŭ kune:",
+    "e18": "mia amiko Tomaso estas tie.",
+    "25": "Sed, kion fari se oni ne povas trovi vorton por io? Nu, oni devus uzi frazon. Pensu, ke la frazo prenas la lokon de nomo: ĝi estas sufiĉe da informo por komuniki ideon, sed per pluraj vortoj. Ni provu:",
+    "e19": "mi ŝatas la ideon ke katoj povu flugi.",
+    "26": "\"la ideon\" (la ideo) ne estas sufiĉe da informo, ĉar ĝi ne diris kio estas la ideo, do ne aldonas frazon kiu diras kio estas la ideo. La eta vorto <em>ke</em> nur diras \"jen frazo\". Estas multo kiun oni povas fari per ĉi tio, do ni provu unu-du aferojn:",
+    "e20": "mi estas kontenta, pro tio ke vi estas ĉi tie.",
+    "27": "La vorto \"tio\" signifas \"tiu afero\", sed kiu afero?! Ĝi ne diras!! Nu, tial ni aldonis frazon por klarigi. Fakte, la \"tio\" ne estas tre interesa, ni eble povus forlasi ĝin. Simile, ni eble ne bezonas la \"pro\" - oni divenus ke ĉi tio estas pri la kaŭzo de io, ĉu ne? Eble ni povus nur diri:",
+    "e21": "mi estas kontenta ke vi estas ĉi tie.",
+    "28": "Sed memoru! Ju pli da vortoj oni forĵetas, des pli la risko ke ĉio eksplodos!",
+    "29": "Dum ni parolas pri forĵeti vortojn, estas alia speco de verba afero, kiu ne vere estas verbo. Se vorto finiĝas per <em>-i</em>, ĝi estas speco de ĝenerala ideo de verbo. La evento ne okazas, ĝi simple iel ekzistas, kaj indikas ke kompleta frazo ekzistas, nedirate. Sed provu priprensi ĉu iu povus miskompreni.",
+    "e22": "la kato volas dormi.",
+    "30": "Kaj finfine estas kelkaj afero kiujn mi ne volas klarigi nun. Ili certe estos iom konfuzaj, kaj ofte temas pri la tre utilaj radikoj <em>ig</em> kaj <em>iĝ</em>.",
+    "e23": "tio interesas min. mi interesiĝas.",
+    "e24": "la kato bluiĝis, ĉar mi bluigis ĝin.",
+    "31": "Kaj, ankaŭ estas participoj, kiuj permesas ke oni uzi agaj konceptoj por priskribi aferoj, ekzemple por diri ke io estas farata. Estas 6 participoj, sed ili sekvas bonan skemon, do vi komprenos. Verŝajne.",
+    "e25": "mi estus farinta tion. mi estos faranta tion. finfine, tio estos farita.",
+    "e26": "trovinte la hundon, mi estas serĉonta la katon.",
+    "32": "Ho, atendu, mi promesis paroli pri tiuj ki- kaj ti- kaj neni- vortoj, kaj mi jam ne faris tion. Fakte mi eĉ neniam menciis neni- antaŭ nun. Bone, jen la komencoj kaj finoj de kelkaj vortoj, simple kunmeti ilin:",
+    "tabelti": "tiu",
+    "tabelki": "kiu",
+    "tabeli": "iu",
+    "tabelneni": "neniu",
+    "tabelcxi": "ĉiu",
+    "tabelo": "afero (ĉi tiu estas substantivo)",
+    "tabelu": "individuo (ĉi tiu estas speco de adjektivo)",
+    "tabela": "speco (ankaŭ adjektivo)",
+    "tabeles": "de individuo (denove, adjektivo)",
+    "tabelel": "maniero (ĉi tiu estas adverbo)",
+    "tabele": "loko (kaj tiel estas la cetero, pli-malpli)",
+    "tabelam": "tempop/kiamo",
+    "tabelal": "kialo",
+    "tabelom": "kvanto/kiomo",
+    "33": "Iel mi forgesis pri kelkaj bazaj vortoj, do ni rigardu kelkajn pluajn frazojn:",
+    "e27": "vi, kaj mi, kaj la kato, estas ĉi tie, sed la hundo mankas.",
+    "e28": "nun, la kato bojas kiel hundo. aŭ eble tio estas la hundo.",
+    "e29": "la lingvo estas facile lernebla, kaj lerninda, kaj baldaŭ estos lernita.",
+    "34": "Jes. Tio sufiĉos.",
+    "end": "Kaj, kio nun? Lernu, uzu, ĝuu! Estas retaj kursoj ĉe <a href=\"https://lernu.net\">lernu.net</a>, kaj ĉe <a href=\"https://www.duolingo.com/\">Duolingo</a>, aŭ ĉe <a href=\"https://learn.esperanto.com/\">Esperanto en 12 tagoj</a>. Aŭ eĉ pli bone, aliĝu al kurso instruata de <a href=\"https://londonaesperantoklubo.com/OnlineEsperantoCourses.html\">La Londona Esperanto-Klubo</a>."
+  },
+  "se": {
+    "title": "Esperanto på 17 minuter"
+  },
+  "pl": {
+    "title": "Esperanto w 17 minut"
+  },
+  "fr": {
+    "title": "Espéranto en 17 minuits"
+  },
+  "de": {
+    "title": "Esperanto in 17 minuten"
+  },
+  "es": {
+    "title": "Esperanto en 17 minutos"
   }
+}
+
+function translate(lang) {
+  let baseText = translations['en']
+
+  let text = translations[lang]
+  if (!text) {
+    // don't have appropriate translation
+    return
+  }
+
+  for (let e of document.querySelectorAll('.learn')) {
+    let id = e.getAttribute('tid')
+    e.innerHTML = text[id] || baseText[id]
+  }
+
+  document.querySelector('.languagechooser select').value = lang
+}
+
+function flag(code) {
+  if (code == 'eo') {
+    // let f = document.createElement('div')
+    // f.classList.add('eoflag')
+    return 'Eo'
+  }
+  if (code == 'en') {
+    code = 'gb'
+  }
+
+  const aL = 'a'.codePointAt(0)
+  const aF = '🇦'.codePointAt(0)
+
+  let out = ''
+  for (let l of code) {
+    let c = l.codePointAt(0)
+    let o = aF + (c - aL)
+    out += String.fromCodePoint(o)
+  }
+
+  return out
+}
+
+function setupLanguageChooser() {
+  let chooser = document.querySelector('.languagechooser')
+  let select = chooser.querySelector('select')
+  for (let lang of Object.keys(translations).sort()) {
+    let opt = document.createElement('option')
+    opt.value = lang
+    opt.append(flag(lang))
+    select.append(opt)
+  }
+  select.addEventListener('change', _e => {
+    let lang = select.value
+    translate(lang)
+  })
+}
+
+function setupAudio(e) {
+  let id = e.getAttribute('fid')
+  let eAudio = document.createElement('audio')
+  eAudio.src = `sound/e${id}.mp3`
+  e.append(eAudio)
+  e.addEventListener('click', _e => {
+    eAudio.play()
+  })
+  eAudio.addEventListener('play', _e => {
+    e.classList.add('playing')
+  })
+  eAudio.addEventListener('ended', _e => {
+    e.classList.remove('playing')
+  })
+  e.append('🔉\ufe0e')
+}
+
+function main() {
+  let en = {}
+  for (let e of document.querySelectorAll('.learn')) {
+    let id = e.getAttribute('tid')
+    en[id] = e.innerHTML
+    translations['en'] = en
+  }
+  console.log(JSON.stringify(en, '', '  '))
+
+  setupLanguageChooser()
+
+  let firstLang = navigator.language.split('-')[0]
+  translate(firstLang)
 
   for (let e of document.querySelectorAll('.listen')) {
     setupAudio(e)
